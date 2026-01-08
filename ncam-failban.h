28a2e27abcbc789b4adce3296937a50acaaf2b9e
speedy005 <67871007+speedy005@users.noreply.github.com>
2026-01-08 18:55:33 +0100

    Update ncam-failban.h

--- ncam-failban.h
+++ ncam-failban.h
@@ -1,6 +1,6 @@
 #ifndef NCAM_FAILBAN_H_
 #define NCAM_FAILBAN_H_
-
+ 
 extern int32_t cs_check_violation(IN_ADDR_T ip, int32_t port);
 int32_t cs_add_violation_by_ip(IN_ADDR_T ip, int32_t port, char *info);
 extern void cs_add_violation(struct s_client *cl, char *info);
