992651b206c07b1970cf83af0f034a988ac68d82
speedy005 <67871007+speedy005@users.noreply.github.com>
2026-01-08 18:54:16 +0100

    Update reader-dre-common.h

--- reader-dre-common.h
+++ reader-dre-common.h
@@ -1,6 +1,6 @@
 #ifndef DRE_COMMON_H_
 #define DRE_COMMON_H_
-
+ 
 	int32_t dre_common_get_emm_type(EMM_PACKET *ep, struct s_reader *rdr);
 	int32_t dre_common_get_emm_filter(struct s_reader *rdr, struct s_csystem_emm_filter **emm_filters, unsigned int *filter_count);
 
