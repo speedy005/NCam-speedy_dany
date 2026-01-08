0f018433a1868628f3a691aec743880228f9c600
speedy005 <67871007+speedy005@users.noreply.github.com>
2026-01-08 19:00:24 +0100

    Update ncam-emm.h

--- ncam-emm.h
+++ ncam-emm.h
@@ -1,6 +1,6 @@
 #ifndef NCAM_EMM_H_
 #define NCAM_EMM_H_
-
+ 
 int32_t emm_reader_match(struct s_reader *reader, uint16_t caid, uint32_t provid);
 void do_emm(struct s_client *client, EMM_PACKET *ep);
 int32_t reader_do_emm(struct s_reader *reader, EMM_PACKET *ep);
